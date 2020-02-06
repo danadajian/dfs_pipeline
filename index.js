64dc7b1c230b1dce1f7edda2afefb7abb6ca8ea3
Dan Adajian <danadajian@gmail.com>
2020-02-05 23:25:33 -0600

    * Finished building and testing data merge lambda

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-exports.handler = function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    return 'test'
+const fs = require('fs');
+const lineupRules = JSON.parse(fs.readFileSync('./lineupRules.json'));
+
+exports.handler = (event, context) => {
+    const sport = event.sport;
+    const fanduelData = event['fanduelData'];
+    const projectionsData = event['projectionsData'];
+    const contestRules = lineupRules['fd'][sport]['Classic'];
+    return {
+        'invocationType': 'pipeline',
+        'lineup': contestRules.lineupPositions.map(position => ({
+            'playerId': 0,
+            'position': position,
+            'team': '',
+            'name': '',
+            'projection': '',
+            'salary': ''
+        })),
+        'playerPool': combineDataIntoPlayerPool(sport, fanduelData, projectionsData),
+        'blackList': [],
+        'lineupPositions': contestRules.lineupPositions,
+        'lineupRestrictions': contestRules.lineupRestrictions,
+        'salaryCap': contestRules.salaryCap,
+        'maxCombinations': 10000000
+    }
 };
 
-exports.getFanduelPlayersFromSport = function(sport, fanduelData) {
-    return fanduelData.filter(
-        contestObject =>
-            contestObject['contest'] === 'Main' &&
-            contestObject['sport'].toLowerCase() === sport)[0]['players']
+const getFanduelPlayersFromSport = (sport, fanduelData) => {
+    return fanduelData.filter(contestObject =>
+        contestObject['contest'] === 'Main' &&
+        contestObject['sport'].toLowerCase() === sport)[0]['players']
 };
 
-// exports.mergeData = function(fanduelData, projectionsData) {
-//     let combinedData = [];
-//     dfsPlayers.forEach(player => {
-//         if (!player.playerId)
-//             player.playerId = parseInt(
-//                 Object.keys(projectionsData)
-//                     .filter(playerId => projectionsData[playerId].name === player.name)[0]
-//             );
-//         let playerData = projectionsData[player.playerId];
-//         if (playerData) {
-//             player.name = playerData.name;
-//             player.team = playerData.team;
-//             player.opponent = playerData.opponent;
-//             player.gameDate = playerData.gameDate;
-//             player.spread = playerData['spread'];
-//             player.overUnder = playerData.overUnder;
-//             player.projection = playerData[site + 'Projection'];
-//             combinedData.push(player);
-//         }
-//     });
-//     return combinedData;
-// };
+const combineDataIntoPlayerPool = (sport, fanduelData, projectionsData) => {
+    let combinedData = [];
+    const fanduelPlayers = getFanduelPlayersFromSport(sport, fanduelData);
+    fanduelPlayers.forEach(player => {
+        let newPlayer = JSON.parse(JSON.stringify(player));
+        if (!player.playerId)
+            newPlayer.playerId = parseInt(
+                Object.keys(projectionsData)
+                    .filter(playerId => projectionsData[playerId].name === player.name)[0]
+            );
+        let playerData = projectionsData[newPlayer.playerId];
+        if (playerData) {
+            newPlayer.projection = playerData['fdProjection'];
+            combinedData.push(newPlayer);
+        }
+    });
+    return combinedData;
+};
+
+exports.getFanduelPlayersFromSport = getFanduelPlayersFromSport;
+exports.combineDataIntoPlayerPool = combineDataIntoPlayerPool;
