0ed8d10ddb7cb838be00df3e253c1b3a4529a3b4
Dan Adajian <danadajian@gmail.com>
2020-02-12 20:00:34 -0600

    * added timezone to getDateString

--- src/scheduleStateMachines/getSlateStartTimes.js
+++ src/scheduleStateMachines/getSlateStartTimes.js
@@ -20,7 +20,7 @@ const getStartTimes = async (sports) => {
 };
 
 const getDateString = () => {
-    const date = new Date();
+    const date = new Date(new Date().toLocaleString("en-US", {timeZone: "America/New_York"}));
     let day = String(date.getDate()).padStart(2, '0');
     let month = String(date.getMonth() + 1).padStart(2, '0');
     let year = date.getFullYear();
