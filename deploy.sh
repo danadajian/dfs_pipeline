eb685970d176561fa080683c90ef2cd507ff06eb
danadajian <danadajian@gmail.com>
2020-04-08 13:14:28 -0400

    * circle

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ fi
 TIMESTAMP=$( date +"%Y-%m-%d_%H-%M-%S" )
 FILE_NAME="dfs-pipeline-js-$TIMESTAMP.zip"
 
-zip "$FILE_NAME" src \
+bestzip "$FILE_NAME" src \
 node_modules/axios node_modules/follow-redirects node_modules/xml2js node_modules/xmlbuilder node_modules/sax \
 node_modules/underscore node_modules/@types node_modules/boolbase node_modules/cheerio node_modules/css-select \
 node_modules/css-what node_modules/domelementtype node_modules/domhandler node_modules/dom-serializer \
