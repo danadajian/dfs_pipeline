ad2b19b47107aeee9ef0be8b0d37c2564f174ee1
Dan Adajian <danadajian@gmail.com>
2020-06-10 17:37:42 -0500

    * migrated getRollingFantasyPointAverages to fantasy_analytics_backend

--- src/getRollingFantasyPointAverages/getCurrentData.ts
+++
@@ -1,5 +0,0 @@
-import {invokeLambdaFunction} from "../aws/aws";
-
-export const getCurrentData = async (sport: string) => {
-    return invokeLambdaFunction(process.env.GET_CURRENT_DATA_LAMBDA_NAME, {sport});
-};
--- src/getRollingFantasyPointAverages/getFantasyData.ts
+++
@@ -1,5 +0,0 @@
-import {invokeLambdaFunction} from "../aws/aws";
-
-export const getFantasyData = async (payload) => {
-    return invokeLambdaFunction(process.env.GET_FANTASY_DATA_LAMBDA_NAME, payload);
-};
