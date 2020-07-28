f759447f6246d95dcdfe6bc28d4ab5f87b48658f
Dan Adajian <danadajian@gmail.com>
2020-07-28 11:29:09 -0500

    * add player projections to publish lineup

--- src/publishOptimalLineup/generateMessage.test.ts
+++ src/publishOptimalLineup/generateMessage.test.ts
@@ -5,29 +5,32 @@ describe('generateMessage', () => {
         const optimalLineupData = {
             'lineup': [
                 {
-                    'name': 'Joe Schmo',
-                    'team': 'CHI',
-                    'position': 'FB'
+                    name: 'Joe Schmo',
+                    team: 'CHI',
+                    position: 'FB',
+                    projection: 69
                 },
                 {
-                    'name': 'John Schmo',
-                    'team': 'NYG',
-                    'position': 'RB'
+                    name: 'John Schmo',
+                    team: 'NYG',
+                    position: 'RB',
+                    projection: 70.69
                 },
                 {
-                    'name': 'Joey Schmo',
-                    'team': 'PHI',
-                    'position': 'QB'
+                    name: 'Joey Schmo',
+                    team: 'PHI',
+                    position: 'QB',
+                    projection: 71.696969
                 }],
             'totalProjection': 69.69696969,
             'totalSalary': 6900
         };
         const result = generateMessage('nfl', optimalLineupData);
         expect(result).toStrictEqual('Good evening. Here is the optimal NFL lineup for tonight:' +
-            '\nJoe Schmo CHI FB' +
-            '\nJohn Schmo NYG RB' +
-            '\nJoey Schmo PHI QB' +
-            '\nTotal projected points: 69.70' +
+            '\n\nJoe Schmo CHI FB\nProjection: 69.00' +
+            '\n\nJohn Schmo NYG RB\nProjection: 70.69' +
+            '\n\nJoey Schmo PHI QB\nProjection: 71.70' +
+            '\n\nTotal projected points: 69.70' +
             '\nTotal salary: $6900'
         );
     });
--- src/publishOptimalLineup/generateMessage.ts
+++ src/publishOptimalLineup/generateMessage.ts
@@ -1,6 +1,8 @@
 export const generateMessage = (sport: string, optimalLineupData: any) => {
     return `Good evening. Here is the optimal ${sport.toUpperCase()} lineup for tonight:` +
-        optimalLineupData.lineup.map(player => `\n${player.name} ${player.team} ${player.position}`).join('')
-        + `\nTotal projected points: ${optimalLineupData.totalProjection.toFixed(2)}`
+        optimalLineupData.lineup.map(player =>
+            `\n\n${player.name} ${player.team} ${player.position}`
+            + `\nProjection: ${player.projection.toFixed(2)}`).join('')
+        + `\n\nTotal projected points: ${optimalLineupData.totalProjection.toFixed(2)}`
         + `\nTotal salary: $${optimalLineupData.totalSalary}`
 };
