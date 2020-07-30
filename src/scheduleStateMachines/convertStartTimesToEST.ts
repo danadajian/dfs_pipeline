d301965cb61324779ebee802c043abe9f5ac6b77
Dan Adajian <danadajian@gmail.com>
2020-07-29 20:52:31 -0500

    * fix est conversion

--- src/scheduleStateMachines/convertStartTimesToEST.ts
+++ src/scheduleStateMachines/convertStartTimesToEST.ts
@@ -3,14 +3,12 @@ import {StartTime} from "../index";
 export const convertStartTimesToEST = (startTimes: StartTime[]) => {
     return startTimes.map((startTime: StartTime, id: number) => {
         const {sport, date} = startTime;
-        const time = date.toLocaleString('en-GB', {
+        const time = date.toLocaleString('en-US', {
             timeZone: 'America/New_York',
             hour: '2-digit',
             minute: '2-digit',
             timeZoneName: 'short'
         });
-        console.log(date);
-        console.log(time);
         return {
             id,
             sport,
