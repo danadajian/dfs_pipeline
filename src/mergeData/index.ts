31ab2a01bb4c0e69ec0fbcd32deec9475694cc7f
Dan Adajian <danadajian@gmail.com>
2020-08-08 13:46:40 -0500

    * improved promise chains

--- src/mergeData/index.ts
+++ src/mergeData/index.ts
@@ -1,37 +1,52 @@
-import {DFS_PIPELINE_BUCKET_NAME, LINEUP_RULES} from "@dadajian/shared-fantasy-constants";
+import {
+    DFS_PIPELINE_BUCKET_NAME,
+    FANTASY_ANALYTICS_BUCKET_NAME,
+    LINEUP_RULES
+} from "@dadajian/shared-fantasy-constants";
 import {combineDataIntoPlayerPool} from './combineDataIntoPlayerPool';
 import {retrieveObjectFromS3, uploadObjectToS3} from "../aws/aws";
+import {getTodayDateString} from "../helpers/helpers";
 
 export const mergeDataHandler = async (event): Promise<any> => {
     let {invocationType, sport, maxCombinations, goalieData = []} = event;
     return Promise.all([
-            retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME, 'fanduelData.json'),
-            retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME,`${sport}ProjectionsData.json`),
+        retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME, 'fanduelData.json'),
+        retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME, `${sport}ProjectionsData.json`),
+    ]).then(([fanduelData, projectionsData]) => {
+        return combineDataIntoPlayerPool(sport, fanduelData, projectionsData, goalieData)
+    }).then(playerPool => {
+        return Promise.all([
+            uploadObjectToS3(playerPool, DFS_PIPELINE_BUCKET_NAME, `${sport}PlayerPool.json`),
+            retrieveObjectFromS3(FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentPlayerPools.json`).catch(() => []),
+            playerPool
         ])
-        .then(([fanduelData, projectionsData]) => {
-            return combineDataIntoPlayerPool(sport, fanduelData, projectionsData, goalieData)
-        })
-        .then(playerPool => {
-            return uploadObjectToS3(playerPool, DFS_PIPELINE_BUCKET_NAME, `${sport}PlayerPool.json`);
-        })
-        .then(() => {
-            const {lineupPositions, lineupRestrictions, salaryCap} = LINEUP_RULES.Fanduel[sport].Classic;
-            return {
-                invocationType,
-                sport,
-                lineup: lineupPositions.map(position => ({
-                    playerId: 0,
-                    position,
-                    team: '',
-                    name: '',
-                    projection: '',
-                    salary: ''
-                })),
-                blackList: [],
-                lineupPositions,
-                lineupRestrictions,
-                salaryCap,
-                maxCombinations
+    }).then(([_, pastPlayerPools, playerPool]) => {
+        const playerPoolData = [
+            {
+                date: getTodayDateString(),
+                playerPool
             }
-        })
+        ];
+        const combinedPlayerPoolData = pastPlayerPools.concat(playerPoolData);
+        return uploadObjectToS3(combinedPlayerPoolData, FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentPlayerPools.json`)
+    }).then(() => {
+        const {lineupPositions, lineupRestrictions, salaryCap} = LINEUP_RULES.Fanduel[sport].Classic;
+        return {
+            invocationType,
+            sport,
+            lineup: lineupPositions.map(position => ({
+                playerId: 0,
+                position,
+                team: '',
+                name: '',
+                projection: '',
+                salary: ''
+            })),
+            blackList: [],
+            lineupPositions,
+            lineupRestrictions,
+            salaryCap,
+            maxCombinations
+        }
+    })
 };
--- src/publishOptimalLineup/index.ts
+++ src/publishOptimalLineup/index.ts
@@ -6,31 +6,26 @@ import {getTodayDateString} from "../helpers/helpers";
 
 export const publishOptimalLineupHandler = async (event): Promise<string> => {
     const {sport} = event;
-    return retrieveObjectFromS3(FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentOptimalLineups.json`)
-        .catch(() => [])
-        .then(pastOptimalLineupData => {
-            return Promise.all([
-                pastOptimalLineupData,
-                retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME, `${sport}OptimalLineup.json`)
-            ])
-        })
-        .then(([pastOptimalLineupData, optimalLineupObject]) => {
-            const optimalLineupData = [
-                {
-                    date: getTodayDateString(),
-                    lineup: optimalLineupObject.lineup
-                }
-            ];
-            const combinedOptimalLineupData = pastOptimalLineupData.concat(optimalLineupData);
-            return Promise.all([
-                uploadObjectToS3(combinedOptimalLineupData, FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentOptimalLineups.json`),
-                optimalLineupObject
-            ])
-        }).then(([_, optimalLineupObject]) => {
-            return generateMessage(sport, optimalLineupObject)
-        }).then((message: string) => {
-            return publishToSnsTopic(message, process.env.OPTIMAL_LINEUP_TOPIC_ARN)
-        }).then(() => {
-            return 'Message published successfully.'
-        });
+    return Promise.all([
+        retrieveObjectFromS3(FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentOptimalLineups.json`).catch(() => []),
+        retrieveObjectFromS3(DFS_PIPELINE_BUCKET_NAME, `${sport}OptimalLineup.json`)
+    ]).then(([pastOptimalLineupData, optimalLineupObject]) => {
+        const optimalLineupData = [
+            {
+                date: getTodayDateString(),
+                lineup: optimalLineupObject.lineup
+            }
+        ];
+        const combinedOptimalLineupData = pastOptimalLineupData.concat(optimalLineupData);
+        return Promise.all([
+            uploadObjectToS3(combinedOptimalLineupData, FANTASY_ANALYTICS_BUCKET_NAME, `${sport}RecentOptimalLineups.json`),
+            optimalLineupObject
+        ])
+    }).then(([_, optimalLineupObject]) => {
+        return generateMessage(sport, optimalLineupObject)
+    }).then((message: string) => {
+        return publishToSnsTopic(message, process.env.OPTIMAL_LINEUP_TOPIC_ARN)
+    }).then(() => {
+        return 'Message published successfully.'
+    });
 };
